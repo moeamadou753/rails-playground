a6c9ac777aa8b934b544aa51243ed0c59e1d963c
GitHub <mohamed.amadou159@gmail.com>
2020-10-12 02:45:02 -0400

    Increase abstraction with partials

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,10 +1,14 @@
 class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
+
     def new
         @article = Article.new
     end
 
-    def index
-        @articles = Article.all
+    def edit
+        @article = Article.find(params[:id])
     end
     
     def create
@@ -14,6 +18,16 @@ class ArticlesController < ApplicationController
             redirect_to @article
         else 
             render 'new'
+        end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render 'edit'
         end
     end
 
